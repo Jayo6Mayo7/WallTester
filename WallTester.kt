3448bf665f60e4ac075b3e1258e6233afbce5a99
Jayo6Mayo7 <Joao_PAlmeidaBastos@caryacademy.org>
2024-09-13 11:45:45 -0400

    Update WallTester.kt
    Cleaned up code, added more detailed text

--- WallTester.kt
+++ WallTester.kt
@@ -1,49 +1,63 @@
 fun main() {
     //w is short for wall
-    var position = mutableListOf(0, 0)
-    var wUp = false
-    var wRight = false
-    var wDown = false
-    var wLeft = false
-    fun wCheck(x: Int,y: Int) {
+    val position = mutableListOf(0, 0)
+    println("Use WASD to move")
+    print("Your location: ")
+    print(position)
+    println()
+    var wUp: Boolean
+    var wRight: Boolean
+    var wDown: Boolean
+    var wLeft: Boolean
+    var ranIntoWall = false
+    fun wCheck(x: Int, y: Int) {
         when (x) {
             -2 -> when (y) {
                 2 -> {
                     wUp = true
                     wLeft = true
                 }
+
                 1 -> {
                     wLeft = true
                     wRight = true
                 }
+
                 0 -> {
                     wLeft = true
                 }
+
                 -1 -> {
                     wLeft = true
                 }
+
                 -2 -> {
                     wLeft = true
                     wDown = true
                     wRight = true
                 }
             }
+
             -1 -> when (y) {
                 2 -> {
                     wUp = true
                     wDown = true
                 }
+
                 0 -> {
                     wUp = true
                 }
+
                 -1 -> {
                     wDown = true
                 }
             }
+
             0 -> when (y) {
                 2 -> {
                     wUp = true
                 }
+
                 1 -> {
                     wLeft = true
                     wRight = true
@@ -53,10 +67,12 @@ fun main() {
                     wDown = true
                 }
             }
+
             1 -> when (y) {
                 2 -> {
                     wUp = true
                 }
+
                 1 -> {
                     wLeft = true
                 }
@@ -64,26 +80,32 @@ fun main() {
                 -1 -> {
                     wDown = true
                 }
+
                 -2 -> {
                     wUp = true
                     wDown = true
                     wLeft = true
                 }
             }
+
             2 -> when (y) {
                 2 -> {
                     wUp = true
                     wRight = true
                 }
+
                 1 -> {
                     wRight = true
                 }
+
                 0 -> {
                     wRight = true
                 }
+
                 -1 -> {
                     wRight = true
                 }
+
                 -2 -> {
                     wRight = true
                     wDown = true
@@ -92,30 +114,46 @@ fun main() {
         }
     }
     while (true) {
-        val move = readLine()
+        val move = readlnOrNull()
         wUp = false
         wRight = false
         wDown = false
         wLeft = false
-        wCheck(position[0],position[1])
+        wCheck(position[0], position[1])
         when (move) {
-            "a" -> if (wLeft == false) {
+            "a" -> if (!wLeft) {
                 position[0] = position[0] - 1
-            } else println("wall")
+            } else {
+                println("You ran into a wall")
+                ranIntoWall = true
+            }
 
-            "d" -> if (wRight == false) {
+            "d" -> if (!wRight) {
                 position[0] = position[0] + 1
-            } else println("wall")
+            } else {
+                println("You ran into a wall")
+                ranIntoWall = true
+            }
 
-            "w" -> if (wUp == false) {
+            "w" -> if (!wUp) {
                 position[1] = position[1] + 1
-            } else println("wall")
+            } else {
+                println("You ran into a wall")
+                ranIntoWall = true
+            }
 
-            "s" -> if (wDown == false) {
+            "s" -> if (!wDown) {
                 position[1] = position[1] - 1
-            } else println("wall")
+            } else {
+                println("You ran into a wall")
+                ranIntoWall = true
+            }
         }
-        print(position)
-        println()
+        if (!ranIntoWall) {
+            print("Your location: ")
+            print(position)
+            println()
+        }
+        ranIntoWall = false
     }
 }
